e7b8ed7b23e0596db293501d5f2f9ba7f7b7f12d
Diego Landony Lima Toledo <132611526+Diego-Landony@users.noreply.github.com>
2024-01-19 15:23:37 -0600

    d

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,10 +1,36 @@
 /* eslint-disable */
 export class HolbertonClass {
-  // ... (unchanged)
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
 }
 
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 export class StudentHolberton {
-  // ... (unchanged)
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
 
   get fullStudentDescription() {
     return `${this.fullName} - ${this.holbertonClass.year} - ${this.holbertonClass.location}`;
